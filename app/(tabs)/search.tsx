4a2b076ee979847b2cbc1b47586d7924785471af
Olivier <olivier@MacBook-Pro-de-Olivier.local>
2025-11-04 02:10:17 +0100

    start to built search term

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from '@/components/SearchBar';
 import { icons } from '@/constants/icons';
 import { images } from '@/constants/images';
 import { fetchMovies } from '@/services/api';
+import { updateSearchCount } from '@/services/appwrite';
 import useFetch from '@/services/useFetch';
 import { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native';
@@ -28,6 +29,8 @@ const search = () => {
 
   // Attendre 500ms avant de lancer la recherche (anti spam)
   useEffect(() => {
+    updateSearchCount(searchQuery, movies[0]);
+
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
